a9b21c3e039ad3549415686a0cde76a25e881e70
Shakshi3104 <51497924+Shakshi3104@users.noreply.github.com>
2021-11-18 12:01:42 +0900

    1st version

--- Sources/Maritozzo/Maritozzo.swift
+++ Sources/Maritozzo/Maritozzo.swift
@@ -1,6 +1,21 @@
-public struct Maritozzo {
-    public private(set) var text = "Hello, World!"
+import Foundation
 
-    public init() {
-    }
+public struct Maritozzo {
+    public let name = "Maritozzo"
+        public let imageURL = URL(string: "https://mldata.lawson.co.jp/recommend/original/detail/img/l642038.png")
+        public let price = 260
+        public let cal = 358
+        public let storeName = "Lawson"
+        public let brandName = "Uchi Café"
+        
+        public var priceString: String {
+            return "¥\(price)"
+        }
+        
+        public var calString: String {
+            return "\(cal) kcal"
+        }
+        
+        public let nameJP = "Uchi Café Spécialité　澄ふわマリトッツォ(ヘーゼルナッツチョコ入り)"
+        public let description = "国産小麦の小麦粉をブレンドしたほろほろふんわりとしたブリオッシュ生地に、北海道産生クリームやマスカルポーネを加えたクリームをはさみました。中のヘーゼルナッツチョコが味わいのアクセントです。"
 }
--- Tests/MaritozzoTests/MaritozzoTests.swift
+++ Tests/MaritozzoTests/MaritozzoTests.swift
@@ -2,10 +2,31 @@ import XCTest
 @testable import Maritozzo
 
 final class MaritozzoTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(Maritozzo().text, "Hello, World!")
-    }
+    func testName() {
+            XCTAssertEqual(Maritozzo().name, "Maritozzo")
+        }
+        
+        func testURL() {
+            XCTAssertNotNil(Maritozzo().imageURL)
+        }
+        
+        func testPrice() {
+            XCTAssertEqual(Maritozzo().price, 260)
+        }
+        
+        func testCal() {
+            XCTAssertEqual(Maritozzo().cal, 358)
+        }
+        
+        func testPriceString() {
+            XCTAssertEqual(Maritozzo().priceString, "¥260")
+        }
+        
+        func testCalString() {
+            XCTAssertEqual(Maritozzo().calString, "358 kcal")
+        }
+        
+        func testBrandName() {
+            XCTAssertEqual(Maritozzo().brandName, "Uchi Café")
+        }
 }
